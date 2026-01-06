ff121ce95927021074981439158c818f521b7ad8
Sounderya22 <vvsounderya22@gmail.com>
2026-01-07 00:35:26 +0530

    Modified some files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
   site: 'https://sounderya22.github.io',
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
